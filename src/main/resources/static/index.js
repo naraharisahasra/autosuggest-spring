2a877864794b0b882cfb59358989e336d3fb461b
sahasra <naraharisahasra@gmail.com>
2026-07-18 12:01:20 +0530

    after toggle user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,43 @@
-// alert("breeeeehhhh")
-console.log("hello from js")
+//  alert("View Profile")
+// console.log("hello from js")
+
+// data = {
+//     "name": "John",
+//     "gender": "Male"
+// }
+
+// data.name //John
+// data.gender //Male
+
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "img": "john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "img": "jane.png"
+    }
+]
+
+var id= 0;
+
+function toggleUser(){
+    id= (id+1)%2
+    //get the elements
+
+    //1:image
+    var userImage=document.getElementById("user-image");
+    userImage.src=users[id].img;
+
+    //2:username
+    var userName=document.getElementById("user-name");
+    userName.innerHTML=users[id].name;
+
+    //3:usergender
+    var userGender=document.getElementById("user-gender");
+    userGender.innerHTML=users[id].gender;
+
+}
