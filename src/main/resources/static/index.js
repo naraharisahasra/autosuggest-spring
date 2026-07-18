625cd796695bbd4de495ec0d24d8901e0c2fcf26
sahasra <naraharisahasra@gmail.com>
2026-07-18 12:49:01 +0530

    after random user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -41,3 +41,29 @@ function toggleUser(){
     userGender.innerHTML=users[id].gender;
 
 }
+
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+
+            var userData=data.results[0];
+
+             //1:image
+            var userImage=document.getElementById("user-image");
+            userImage.src=userData.picture.large;
+
+            //2:username
+            var userName=document.getElementById("user-name");
+            userName.innerHTML=userData.name.first + " " + userData.name.last;
+
+            //3:usergender
+            var userGender=document.getElementById("user-gender");
+            userGender.innerHTML=userData.gender;
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    }
